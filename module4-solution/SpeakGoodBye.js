da562cbb90d1dadc649ad026d3e80e45864992a6
Yurii Antipov <yuriy.antipov85@gmail.com>
2016-10-23 23:27:16 +0300

    Add module 4 solution

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function (window) {
+    var byeSpeaker = {};
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    };
+
+    var speakWord = "Good Bye";
+
+    window.byeSpeaker = byeSpeaker;
+})(window);
+
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,11 @@
+(function (window) {
+    var helloSpeaker = {};
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    };
+
+    var speakWord = "Hello";
+
+    window.helloSpeaker = helloSpeaker;
+})(window);
+
